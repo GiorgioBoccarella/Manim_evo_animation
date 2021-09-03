53d41aa07dc81d5fe4784d7430d4943297b07469
Giorgio Boccarella <giorgioboccarella@gmail.com>
2021-09-03 15:41:00 +0200

    Dynamic_impl

--- common.py
+++ common.py
@@ -1,4 +1,5 @@
 import math
+import manim
 import numpy as np
 import perlin_noise as pns
 
@@ -8,74 +9,132 @@ res = 4
 
 np.random.rand(4)
 
+
 class Ind:
     def __init__(self, coord, id, fitness):
         self.coord = coord
         self.id = id
         self.fitness = fitness
-        
+
+
 def make_ind(features):
-            coord, id, fit = features
-            return Ind(coord, id, fit)        
-        
+    coord, id, fit = features
+    return Ind(coord, id, fit)
+
+
 def add_to_archive(individual, archive):
-            archive[individual.id] = individual
-            return 1     
-        
+    archive[individual.id] = individual
+    return 1
+
+
+def create_color_list():
+
+    list = []
+
+    my_res = 1000
+    my_u_bound = 0.55
+    my_l_bound = -0.15
+
+    interval_1 = np.linspace(my_l_bound, -0.0000001, num=my_res)
+    interval_2 = np.linspace(-0.0000001, my_u_bound, num=my_res)
+
+    interval_red = np.linspace(253, 33, num=my_res, dtype=int)
+    interval_green = np.linspace(231, 145, num=my_res, dtype=int)
+    interval_blue = np.linspace(37, 140, num=my_res, dtype=int)
+
+    interval_red2 = np.linspace(33, 68, num=my_res, dtype=int)
+    interval_green2 = np.linspace(145, 1, num=my_res, dtype=int)
+    interval_blue2 = np.linspace(140, 84, num=my_res, dtype=int)
+
+    for i in range(my_res):
+        list.append(
+            tuple(
+                (
+                    manim.rgb_to_color(
+                        [
+                            interval_red[i] / 255,
+                            interval_green[i] / 255,
+                            interval_blue[i] / 255,
+                        ]
+                    ),
+                    interval_1[i],
+                )
+            )
+        )
+
+    for i in range(my_res):
+        list.append(
+            tuple(
+                (
+                    manim.rgb_to_color(
+                        [
+                            interval_red2[i] / 255,
+                            interval_green2[i] / 255,
+                            interval_blue2[i] / 255,
+                        ]
+                    ),
+                    interval_2[i],
+                )
+            )
+        )
+
+    my_list = list
+
+    return my_list
+
+
 def param_gauss(u, v):
-            x = u
-            y = v
-            d = np.sqrt(x * x + y * y)
-            sigma, mu = 0.4, 0.0
-            z = np.exp(-((d - mu) ** 2 / (2.0 * sigma ** 2)))
-            return np.array([x , y, z*0.6])
-        
-        
-def param_gauss_mod(u, v):
     x = u
     y = v
-    z = 3*(1-x)**2.*math.exp(-(x**2) - (y+1)**2) - 10*(x/5 - x**3 - y**5)*math.exp(-x**2-y**2) - 1/3*math.exp(-(x+1)**2 - y**2) 
-    return np.array([x+0.25 , y+ 0.25, z*0.28])
+    d = np.sqrt(x * x + y * y)
+    sigma, mu = 0.4, 0.0
+    z = np.exp(-((d - mu) ** 2 / (2.0 * sigma ** 2)))
+    return np.array([x, y, z * 0.6])
 
 
+def param_multi_mod(u, v):
+    x = u
+    y = v
+    z = (
+        3 * (1 - x) ** 2.0 * math.exp(-(x ** 2) - (y + 1) ** 2)
+        - 10 * (x / 5 - x ** 3 - y ** 5) * math.exp(-(x ** 2) - y ** 2)
+        - 1 / 3 * math.exp(-((x + 1) ** 2) - y ** 2)
+    )
+    return np.array([x + 0.25, y + 0.25, z * 0.28])
 
-def param_gauss_mod_find(u, v, dt=1):
+
+p2 = pns.PerlinNoiseFactory(2)
+
+
+def param_noisy_func(u, v, dt=1):
     np.random.seed(dt)
     x = u
     y = v
     z = p2.get_plain_noise(u, v) + np.random.uniform(0, 2)
-    z_1 = 3*(1-x)**2.*math.exp(-(x**2) - (y+1)**2) - 10*(x/5 - x **
-                                                            3 - y**5)*math.exp(-x**2-y**2) - 1/3*math.exp(-(x+1)**2 - y**2)
-    return (x, y, z*0.4 + z_1*0.3)
-        
+    z_1 = (
+        3 * (1 - x) ** 2.0 * math.exp(-(x ** 2) - (y + 1) ** 2)
+        - 10 * (x / 5 - x ** 3 - y ** 5) * math.exp(-(x ** 2) - y ** 2)
+        - 1 / 3 * math.exp(-((x + 1) ** 2) - y ** 2)
+    )
+    return (x, y, z * 0.4 + z_1 * 0.3)
+
+
 def my_func_comp(u, v):
-            w = np.sin(.2 * np.cos(u ** 2) + np.sin(u) + np.cos(v))
-            return np.array([u, v, (w)*1.2])
-        
-        
+    w = np.sin(0.2 * np.cos(u ** 2) + np.sin(u) + np.cos(v))
+    return np.array([u, v, (w) * 1.2])
+
+
 def my_func_comp_find(u, v):
-            w = .1 * u ** 2 + np.sin(u) + np.cos(v) + 2
-            return (u, v, (w -2.5)*0.45)
-        
-        
-def find_z_from_coord(u, v):
-        x = u
-        y = v
-        d = np.sqrt(x * x + y * y)
-        sigma, mu = 0.4, 0.0
-        z = np.exp(-((d - mu) ** 2 / (2.0 * sigma ** 2)))
-        return (x, y, z*0.6)
-    
-def add_to_archive(individual, archive):
-            archive[individual.id] = individual
-            return 1
-        
+    w = 0.1 * u ** 2 + np.sin(u) + np.cos(v) + 2
+    return (u, v, (w - 2.5) * 0.45)
+
+
 def mutate_uni(archive_dict, mutation_step):
-                    
+
     for ind_in_archive in range(0, len(archive_dict)):
         x, y, z = archive_dict[ind_in_archive].coord
-        x_ran = np.random.uniform(-mutation_step , mutation_step)
-        y_ran = np.random.uniform(-mutation_step , mutation_step)
+        x_ran = np.random.uniform(-mutation_step, mutation_step)
+        y_ran = np.random.uniform(-mutation_step, mutation_step)
         x_m = x + x_ran
         y_m = y + y_ran
         # Check they do not move outside the u,v
@@ -84,44 +143,21 @@ def mutate_uni(archive_dict, mutation_step):
             archive_dict[ind_in_archive].coord = new_x, new_y, new_z
     return 1
 
+
 def mutate_cauchy(archive_dict, dt, prob):
-                    
+
     for ind_in_archive in range(0, len(archive_dict)):
         n, p = 1, prob  # number of trials, probability of each trial
         s = int(np.random.binomial(n, p, 1))
         if s > 0:
             x, y, z = archive_dict[ind_in_archive].coord
-            mu, sigma = 0, 0.47 # mean and standard deviation
+            mu, sigma = 0, 0.47  # mean and standard deviation
             x_ran = float(np.random.normal(mu, sigma, 1))
             y_ran = float(np.random.normal(mu, sigma, 1))
             x_m = x + x_ran
             y_m = y + y_ran
             # Check they do not move outside the u,v
             if -res < x_m < res and -res < y_m < res:
-                new_x, new_y, new_z = param_gauss_mod_find(x_m, y_m, dt)
+                new_x, new_y, new_z = param_noisy_func(x_m, y_m, dt)
                 archive_dict[ind_in_archive].coord = new_x, new_y, new_z
     return 1
-
-def mutate_2(archive_dict, prob):
-                    
-    for ind_in_archive in range(0, len(archive_dict)):
-        n, p = 1, prob  # number of trials, probability of each trial
-        s = int(np.random.binomial(n, p, 1))
-        if s > 0:
-            x, y, z = archive_dict[ind_in_archive].coord
-            mu, sigma = 0, 0.95 # mean and standard deviation
-            x_ran = float(np.random.normal(mu, sigma, 1))
-            y_ran = float(np.random.normal(mu, sigma, 1))
-            x_m = x + x_ran
-            y_m = y + y_ran
-            # Check they do not move outside the u,v
-            if -res < x_m < res and -res < y_m < res:
-                new_x, new_y, new_z = param_gauss_mod_find(x_m, y_m)
-                archive_dict[ind_in_archive].coord = new_x, new_y, new_z
-    return 1
-
-
-
-
-
-
